8d663df8664b780795fde859a46b8c39e2cabda7
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2018-02-20 12:57:33 +0300

    to-do list or Edit form rendered depending on the current "mode"

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import ToDoList from './ToDoList';
 import EditItem from './EditItem';
 
+const LIST_MODE = 'LIST_MODE';
+const EDIT_MODE = 'EDIT_MODE';
+
 export default class App extends Component {
     constructor(props) {
         super(props);
@@ -10,23 +13,37 @@ export default class App extends Component {
                 {id: 1, title: 'Task 1'},
                 {id: 2, title: 'Task 2'},
                 {id: 3, title: 'Task 3'}
-            ]
+            ],
+            mode: LIST_MODE
         };
         this.addItem = this.addItem.bind(this);
+        this.saveItem = this.saveItem.bind(this);
     }
     
     render() {
         return <div>
             <h1>To-Do list!</h1>
-            <ToDoList list={this.state.list}/>
-            <EditItem submit={this.addItem}/>
+            {this.state.mode === LIST_MODE &&
+             <div>
+                 <ToDoList list={this.state.list}/>
+                 <button onClick={this.addItem}>Add</button>
+             </div>
+            }
+            {this.state.mode === EDIT_MODE && <EditItem submit={this.saveItem}/>}
         </div>;
     }
 
-    addItem(title) {
+    addItem() {
+        this.setState({ mode: EDIT_MODE } );
+    }
+
+    saveItem(title) {
         let list = [...this.state.list];
         let newId = 1 + list.reduce((res, v) => Math.max(v, res), 0);
         list.push({id: newId, title: title});
-        this.setState({list: list});
+        this.setState({
+            list: list,
+            mode: LIST_MODE
+        });
     }
 }
